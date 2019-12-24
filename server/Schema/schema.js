298ba23b775410ce7ef79d3efba38ca8ceb2f4c9
Aijaz12550 <eparis@atlassian.com>
2019-12-24 19:35:08 +0500

    relation b/w two tables

--- server/Schema/schema.js
+++ server/Schema/schema.js
@@ -8,22 +8,57 @@ const {
 } = graphql;
 
 const Book = [
-    { name: 'first', id: "1" },
-    { name: 'second', id: "2" },
-    { name: 'third', id: "3" },
+    { name: 'first', id: "1",authorId:"2" },
+    { name: 'second', id: "2",authorId:"1" },
+    { name: 'third', id: "3",authorId:"3" },
 ]
 
+const Author = [
+    { name: 'firstAuthor', id: "1" },
+    { name: 'Aijaz', id: "2"},
+    { name: 'Ahmed', id: "3" },
+]
+
+
+// schema----1
 const BookType = new GraphQLObjectType({
     name: "Book",
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        author:{
+            type:AuthorType,
+            resolve(parent,args){
+                  console.log("parent ==>",parent)
+                return _.find(Author,{id:parent.authorId})
+            }
+        }
+    })
+})
+
+// schema -- 2
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
     fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
     })
 })
 
+
+
+
+// ----------------------query 
+
+
+// ------
+
+
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
+
+        // -------------------------------------book
         Book: {
             type: BookType,
             args: { id: { type: GraphQLString } },
@@ -31,8 +66,19 @@ const RootQuery = new GraphQLObjectType({
                 return _.find(Book, { id: args.id })
             }
         },
+
+        // ---------------------------author
+
+        Author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent,args){
+                return _.find(Author, {id:args.id})
+            }
+        } 
     }
 })
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
